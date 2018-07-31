09e3fcbddc1f8bdab2b0043f3d0434a60013ffba
Dennis Cardinaels <dennis.cardinaels@gmail.com>
2018-07-31 11:05:24 +0200

    added thresholds backend hr

---
+++ src/backend/models/thresholds/heart.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const heartThresholdSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    warningLess: Number,
+    warningHigher: Number,
+    dangerLess: Number,
+    dangerHigher: Number
+});
+
+module.exports = mongoose.model('HeartThreshold', heartThresholdSchema);
--- src/backend/routes/heart.js
+++ src/backend/routes/heart.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-const Heart = require('../models/heart')
+const Heart = require('../models/heart');
+const HeartThreshold = require('../models/thresholds/heart');
 
 router.get('/', (req, res, next) => {
     var userId = req.originalUrl.split('/')[2];
@@ -49,9 +50,6 @@ router.get('/small/:start&:end', (req, res, next) => {
             });
         })
         .catch();
-
-
-    
 });
 
 router.post('/', (req, res, next) => {
@@ -73,6 +71,62 @@ router.post('/', (req, res, next) => {
             res.status(500).json({
                 error: err
             });
+        });
+});
+
+router.post('/threshold', (req, res, next) => {
+    var userId = req.originalUrl.split('/')[2];
+    const heartThreshold = new HeartThreshold({
+        _id: mongoose.Types.ObjectId(),
+        user: userId,
+        warningLess: 55,
+        warningHigher: 75,
+        dangerLess: 45,
+        dangerHigher: 85
+    });
+    heartThreshold
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+router.patch('/threshold', (req, res, next) => {
+    var userId = req.originalUrl.split('/')[2];
+    HeartThreshold.update({ user: userId }, { $set: { warningLess: req.body.warningLess, warningHigher: req.body.warningHigher, dangerLess: req.body.dangerLess, dangerHigher: req.body.dangerHigher }})
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+router.get('/threshold', (req, res, next) => {
+    var userId = req.originalUrl.split('/')[2];
+
+    HeartThreshold.find({ user: userId, })
+        .select("warningLess warningHigher dangerLess dangerHigher")
+        .exec()
+        .then(doc => {
+            console.log(doc);
+            res.status(200).json(doc);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
         });
 });
 
