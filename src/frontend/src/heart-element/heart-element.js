6a888612be507a930eebd2e64a6bf4795c4f7d48
Dennis Cardinaels <dennis.cardinaels@gmail.com>
2018-08-03 16:13:45 +0200

    code cleanup + refactor

--- src/frontend/src/heart-element/heart-element.js
+++ src/frontend/src/heart-element/heart-element.js
@@ -28,11 +28,6 @@ class HeartElement extends PolymerElement {
                 margin-bottom: 5px;
             }
 
-            .block {
-                display: inline-block;
-                margin: auto;
-            }
-
             .c3-region.red {
                 fill: red;
             }
@@ -187,9 +182,6 @@ class HeartElement extends PolymerElement {
                 type: String,
                 value: "day"
             },
-            chart: {
-                type: Object
-            },
             low: {
                 type: String,
                 value: "?"
@@ -219,11 +211,23 @@ class HeartElement extends PolymerElement {
 
     ready() {
         super.ready();
+        
+        this.setUserId();
+        this.setDates();
+
+        this.$.day.style.background = "#cac9c9";
+
+        this.$.ajaxHeart.generateRequest();
+        this.$.ajaxHeartSmall.generateRequest();
+    }
+
+    setUserId() {
         var split = document.URL.split("/");
         var param = split[split.length - 1];
         this.userId = param;
-        this.$.day.style.background = "#cac9c9";
+    }
 
+    setDates() {
         this.endDate = new Date(this.endDate);
         this.endDate.setHours(23);
         this.endDate.setMinutes(59);
@@ -238,39 +242,27 @@ class HeartElement extends PolymerElement {
 
         this.startInt = this.startDate.getTime();
         this.endInt = this.endDate.getTime();
-
-        this.$.ajaxHeart.generateRequest();
-        this.$.ajaxHeartSmall.generateRequest();
     }
 
     dateClick(e) {
-        this.$.day.style.background = "#e0e0e0";
-        this.$.week.style.background = "#e0e0e0";
-        this.$.month.style.background = "#e0e0e0";
-
-        e.srcElement.style.background = "#cac9c9";
-
+        this.setActiveButton(e.srcElement);
+        
         var id = e.srcElement.id;
         this.curPressed = id;
+        var newDate = new Date();
         if (id === "day") {
-            var newDate = new Date();
             newDate.setTime(this.endDate.getTime() - (24 * 60 * 60 * 1000 * 3)+1000);
             this.startDate.setTime(newDate.getTime());
             this.startDateStr = this.getDateString(this.startDate);
         } else if (id === "week") {
-            var newDate = new Date();
             newDate.setTime(this.endDate.getTime() - (24 * 60 * 60 * 1000 * 7)+1000);
             this.startDate.setTime(newDate.getTime());
             this.startDateStr = this.getDateString(this.startDate);
         } else if (id === "month") {
-            var newDate = new Date();
             newDate.setTime(this.endDate.getTime() - (24 * 60 * 60 * 1000 * 28)+1000);
             this.startDate.setTime(newDate.getTime());
             this.startDateStr = this.getDateString(this.startDate);
         }
-
-        this.chart.axis.min({ x: this.startDateStr });
-        this.chart.axis.max({ x: this.endDateStr });
 
         this.startInt = this.startDate.getTime();
         this.endInt = this.endDate.getTime();
@@ -279,18 +271,21 @@ class HeartElement extends PolymerElement {
         this.$.ajaxHeartSmall.generateRequest();
     }
 
+    setActiveButton(element) {
+        this.$.day.style.background = "#e0e0e0";
+        this.$.week.style.background = "#e0e0e0";
+        this.$.month.style.background = "#e0e0e0";
+
+        element.style.background = "#cac9c9";
+    }
+
     changeDate(e) {
         var id = e.srcElement.id;
 
         var operator = 0;
-
-        if (this.curPressed === "day") {
-            operator = 3;
-        } else if (this.curPressed === "week") {
-            operator = 7;
-        } else if (this.curPressed === "month") {
-            operator = 28;
-        }
+        if (this.curPressed === "day")          operator = 3;
+        else if (this.curPressed === "week")    operator = 7;
+        else if (this.curPressed === "month")   operator = 28;
 
         if (id === "back") {
             this.startDate.setTime(this.startDate.getTime() - (24 * 60 * 60 * 1000 * operator));
@@ -324,9 +319,11 @@ class HeartElement extends PolymerElement {
             dateArray.push(this.getDateString(date));
         }
 
-        var ticks = this.getTicks();
+        this.generateChart(dateArray, valArray, thresholds);
+    }
 
-        this.chart = c3.generate({
+    generateChart(dateArray, valArray, thresholds) {
+        var chart = c3.generate({
             bindto: this.$.chart,
             padding: {
                 right: 10
@@ -346,7 +343,7 @@ class HeartElement extends PolymerElement {
                     type: 'timeseries',
                     tick: {
                         format: '%d/%m',
-                        values: ticks
+                        values: this.getTicks()
                     }
                 },
                 y: {
@@ -363,7 +360,6 @@ class HeartElement extends PolymerElement {
         });
     }
 
-
     dataReceivedSmall(event) {
         var stats = event.detail.response;
         this.low = stats.low;
@@ -373,35 +369,20 @@ class HeartElement extends PolymerElement {
         this.warningVals = stats.warningVals;
         this.okVals = stats.okVals;
 
-        if (stats.lowCol === "red") {
-            this.$.lowCell.style.backgroundColor = "#ff9999";
-        } else if (stats.lowCol === "yellow") {
-            this.$.lowCell.style.backgroundColor = "#ffff80";
-        } else if (stats.lowCol === "green") {
-            this.$.lowCell.style.backgroundColor = "#4dff88";
-        } else {
-            this.$.lowCell.style.backgroundColor = "";
-        }
+        if (stats.lowCol === "red")         this.$.lowCell.style.backgroundColor = "#ff9999"
+        else if (stats.lowCol === "yellow") this.$.lowCell.style.backgroundColor = "#ffff80";
+        else if (stats.lowCol === "green")  this.$.lowCell.style.backgroundColor = "#4dff88";
+        else                                this.$.lowCell.style.backgroundColor = "";
 
-        if (stats.avgCol === "red") {
-            this.$.avgCell.style.backgroundColor = "#ff9999";
-        } else if (stats.avgCol === "yellow") {
-            this.$.avgCell.style.backgroundColor = "#ffff80";
-        } else if (stats.avgCol === "green") {
-            this.$.avgCell.style.backgroundColor = "#4dff88";
-        } else {
-            this.$.avgCell.style.backgroundColor = "";
-        }
+        if (stats.avgCol === "red")         this.$.avgCell.style.backgroundColor = "#ff9999";
+        else if (stats.avgCol === "yellow") this.$.avgCell.style.backgroundColor = "#ffff80";
+        else if (stats.avgCol === "green")  this.$.avgCell.style.backgroundColor = "#4dff88";
+        else                                this.$.avgCell.style.backgroundColor = "";
 
-        if (stats.highCol === "red") {
-            this.$.highCell.style.backgroundColor = "#ff9999";
-        } else if (stats.highCol === "yellow") {
-            this.$.highCell.style.backgroundColor = "#ffff80";
-        } else if (stats.highCol === "green") {
-            this.$.highCell.style.backgroundColor = "#4dff88";
-        } else {
-            this.$.highCell.style.backgroundColor = "";
-        }
+        if (stats.highCol === "red")        this.$.highCell.style.backgroundColor = "#ff9999";
+        else if (stats.highCol === "yellow")this.$.highCell.style.backgroundColor = "#ffff80";
+        else if (stats.highCol === "green") this.$.highCell.style.backgroundColor = "#4dff88";
+        else                                this.$.highCell.style.backgroundColor = "";
     }
 
     getTicks() {
