5d3cd1c32ce3259c7048f0a0238df1b30ecfba27
Dennis Cardinaels <dennis.cardinaels@gmail.com>
2018-09-24 21:41:45 +0200

    stronger user model by requiring values

--- src/backend/models/user.js
+++ src/backend/models/user.js
@@ -2,14 +2,49 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    firstName: String,
-    lastName: String,
-    birth: Date,
-    gender: String,
-    bloodType: String,
-    height: String,
-    address: String,
-    phone: String
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    nid: {
+        type: String,
+        required: true
+    },
+    birth: {
+        type: Date,
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: ['M', 'F'],
+        required: true
+    },
+    bloodType: {
+        type: String,
+        enum: ['O+', 'O-', 'A+', 'A-', 'B+', 'B-', 'AB+', 'AB-'],
+        required: true
+    },
+    height: {
+        type: Number,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    smoker: {
+        type: Boolean,
+        required: false,
+        default: false
+    },
 });
 
 module.exports = mongoose.model('User', userSchema);
